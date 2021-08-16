b13c00e5b1145b48254e3386c82449bdfed4adb4
TheGamingSnail <87595446+TheGamingSnail@users.noreply.github.com>
2021-08-17 00:15:16 +0300

    ugh ctrl v is a life saver

--- src/GamingSnail7410/ReportUI/Main.php
+++ src/GamingSnail7410/ReportUI/Main.php
@@ -48,40 +48,40 @@ class Main extends PluginBase {
                       $p->sendMessage("New Report Has Been Sent\nName: " . $data[0] . "\nReason: " . $data[1] . "\nReporter: " . $player->getName() . "\nBan?");        
               }
 // Construct a discord webhook with its URL
-$webHook = new Webhook("YOUR WEBHOOK URL");
+              $webHook = new Webhook("YOUR WEBHOOK URL");
 
 // Construct a new Message object
-$msg = new Message();
+              $msg = new Message();
 
-$msg->setUsername("USERNAME");
-$msg->setAvatarURL("https://cortexpe.xyz/utils/kitsu.png");
-$msg->setContent("INSERT TEXT HERE");
+              $msg->setUsername("USERNAME");
+              $msg->setAvatarURL("https://cortexpe.xyz/utils/kitsu.png");
+              $msg->setContent("INSERT TEXT HERE");
 
 // Create an embed object with #FF0000 (RED) as the embed's color and "EMBED 1" as the title
-$embed = new Embed();
-$embed->setTitle("EMBED 1");
-$embed->setColor(0xFF0000);
-$msg->addEmbed($embed);
+              $embed = new Embed();
+              $embed->setTitle("EMBED 1");
+              $embed->setColor(0xFF0000);
+              $msg->addEmbed($embed);
 
-$embed = new Embed();
-$embed->setTitle("EMBED 2");
-$embed->setColor(0x00FF00);
-$embed->setAuthor("AUTHOR", "https://CortexPE.xyz", "https://cortexpe.xyz/utils/kitsu.png");
-$embed->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
-$msg->addEmbed($embed);
+              $embed = new Embed();
+              $embed->setTitle("EMBED 2");
+              $embed->setColor(0x00FF00);
+              $embed->setAuthor("AUTHOR", "https://CortexPE.xyz", "https://cortexpe.xyz/utils/kitsu.png");
+              $embed->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+              $msg->addEmbed($embed);
 
-$embed = new Embed();
-$embed->setTitle("EMBED 3");
-$embed->setColor(0x0000FF);
-$embed->addField("FIELD ONE", "Some text here");
-$embed->addField("FIELD TWO", "Some text here", true);
-$embed->addField("FIELD THREE", "Some text here", true);
-$embed->setThumbnail("https://cortexpe.xyz/utils/kitsu.png");
-$embed->setImage("https://cortexpe.xyz/utils/kitsu.png");
-$embed->setFooter("Erin is kawaii UwU","https://cortexpe.xyz/utils/kitsu.png");
-$msg->addEmbed($embed);
+              $embed = new Embed();
+              $embed->setTitle("EMBED 3");
+              $embed->setColor(0x0000FF);
+              $embed->addField("FIELD ONE", "Some text here");
+              $embed->addField("FIELD TWO", "Some text here", true);
+              $embed->addField("FIELD THREE", "Some text here", true);
+              $embed->setThumbnail("https://cortexpe.xyz/utils/kitsu.png");
+              $embed->setImage("https://cortexpe.xyz/utils/kitsu.png");
+              $embed->setFooter("Erin is kawaii UwU","https://cortexpe.xyz/utils/kitsu.png");
+              $msg->addEmbed($embed);
 
-$webHook->send($msg);
+              $webHook->send($msg);
           });
            $form->setTitle("Report A Player");
            $form->addInput("Type In A Player Name");
